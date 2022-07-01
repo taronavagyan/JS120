ba90871e7241659cae7614e4316f1cbf4b6ab1a5
Taron Avagyan <contact@taronavagyan.com>
2022-07-01 15:09:19 -0700

    first

---
+++ oo_rps.js
@@ -0,0 +1,26 @@
+function createPlayer() {
+  return {
+    // possible state: player's name?
+    // possible state: player's current move?
+
+    choose() {
+      // not yet implemented
+    },
+  };
+}
+
+function createMove() {
+  return {
+    // possible state: type of move (rock, paper, or scissors)
+  };
+}
+
+function createRule() {
+  return {
+    // possible state? not clear whether Rules need state
+  };
+}
+
+let compare = function (move1, move2) {
+  // not yet implemented
+};
