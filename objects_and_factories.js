fb0febb0b52d76cf13dc4d280047a2530c6e6f0e
Taron Avagyan <contact@taronavagyan.com>
2022-07-02 10:24:55 -0700

    minor

--- objects_and_factories.js
+++ objects_and_factories.js
@@ -18,11 +18,6 @@ function createBook(title, author, read = false) {
 }
 
 let book1 = createBook("Mythos", "Stephen Fry");
-
-let book2 = createBook("Me Talk Pretty One Day", "David Sedaris");
-
-let book3 = createBook("Aunts aren't Gentlemen", "PG Wodehouse");
-
 console.log(book1.getDescription());
 book1.readBook();
 console.log(book1.getDescription());
