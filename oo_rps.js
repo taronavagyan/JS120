83095a6670092a5feb477a11a1ba44aab9e27c97
Taron Avagyan <contact@taronavagyan.com>
2022-07-01 17:05:51 -0700

    game and player objects

--- oo_rps.js
+++ oo_rps.js
@@ -1,7 +1,31 @@
-function createPlayer() {
+const RPSGAME = {
+  human: createPlayer("human"),
+  computer: createPlayer("computer"),
+
+  displayWelcomeMessage() {
+    console.log(`Welcome to Rock, Paper, Scissors!`);
+  },
+
+  displayGoodbyeMessage() {
+    console.log(`Thanks for playing Rock, Paper, Scissors. Goodbye!`);
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    this.human.choose();
+    this.computer.choose();
+    displayWinner();
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSGAME.play();
+
+function createPlayer(playerType) {
   return {
     // possible state: player's name?
     // possible state: player's current move?
+    playerType,
 
     choose() {
       // not yet implemented
