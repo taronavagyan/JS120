0726ff3ac7aefe977e978cc8c88e572e06eaa91c
Taron Avagyan <contact@taronavagyan.com>
2022-07-07 14:46:44 -0700

    function declaration test

---
+++ functions.js
@@ -0,0 +1,11 @@
+// NOTE: Run this code from a file; don't use the REPL
+
+bar();
+function bar() {
+  console.log("this is bar");
+}
+
+foo();
+const foo = function () {
+  console.log("this is foo");
+};
