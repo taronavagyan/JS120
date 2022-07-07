84ad74b550c196239aac3572daba318c82271f73
Taron Avagyan <contact@taronavagyan.com>
2022-07-07 16:21:55 -0700

    minor fixes

--- oo_rps.js
+++ oo_rps.js
@@ -11,8 +11,52 @@ const WINNING_COMBOS = {
 const RPSGAME = {
   human: createHuman(),
   computer: createComputer(),
+  matchLength: 0,
 
-  updateComputerChoices() {
+  play() {
+    this.displayWelcomeMessage();
+    while (true) {
+      this.resetGame();
+      this.updateMatchLength();
+      console.clear();
+      while (this.getLeadingScore() < this.matchLength) {
+        this.human.choose();
+        this.computer.choose();
+        this.improveComputerPlay();
+        console.clear();
+        this.displayWinner();
+        this.displayScore();
+      }
+      if (!this.playAgain()) break;
+    }
+    this.displayGoodbyeMessage();
+  },
+
+  resetGame() {
+    this.human.resetPlay();
+    this.computer.resetPlay();
+  },
+
+  getLeadingScore() {
+    return this.human.score > this.computer.score
+      ? this.human.score
+      : this.computer.score;
+  },
+
+  updateMatchLength() {
+    while (true) {
+      let matchLength = Number(
+        readline.question("How many points does the winner need to score? ")
+      );
+      if (Number.isInteger(matchLength) && matchLength > 0) {
+        this.matchLength = matchLength;
+        break;
+      }
+      console.log("Please enter an integer greater than 0.");
+    }
+  },
+
+  improveComputerPlay() {
     let counterMove = Object.keys(WINNING_COMBOS).find((move) =>
       WINNING_COMBOS[move].includes(this.human.move)
     );
@@ -48,7 +92,7 @@ const RPSGAME = {
 
   displayScore() {
     console.log("------");
-    console.log("Score");
+    console.log("Score:");
     console.log("------");
     console.log(`You: ${this.human.score} Computer: ${this.computer.score}`);
   },
@@ -57,23 +101,6 @@ const RPSGAME = {
     console.log("Would you like to play again? (y/n)");
     let answer = readline.question();
     return answer.toLowerCase()[0] === "y";
-  },
-
-  play() {
-    this.displayWelcomeMessage();
-    while (true) {
-      this.human.choose();
-      this.computer.choose();
-      this.updateComputerChoices();
-      console.clear();
-      this.displayWinner();
-      this.displayScore();
-      if (!this.playAgain()) break;
-    }
-
-    console.log(this.human.moveHistory);
-    console.log(this.computer.moveHistory);
-    this.displayGoodbyeMessage();
   },
 };
 
@@ -90,6 +117,12 @@ function createPlayer() {
 
     countWin() {
       this.score += 1;
+    },
+
+    resetPlay() {
+      this.score = 0;
+      this.moveHistory = [];
+      this.choices = Object.keys(WINNING_COMBOS);
     },
 
     updateMoveHistory(move) {
@@ -123,7 +156,7 @@ function createComputer() {
   let playerObject = createPlayer();
 
   let computerObject = {
-    // add a "counter" to human's last choice as a choice option
+    // add a "counter move" to human's last choice as a choice option
     choose() {
       let randomIndex = Math.floor(Math.random() * this.choices.length);
       let choice = this.choices[randomIndex];
