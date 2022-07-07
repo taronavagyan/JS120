3a81803a62731f5430fc9df2024d92e67f190409
Taron Avagyan <contact@taronavagyan.com>
2022-07-07 15:52:53 -0700

    practice prob

--- functions.js
+++ functions.js
@@ -1,11 +1,29 @@
 // NOTE: Run this code from a file; don't use the REPL
 
-bar();
-function bar() {
-  console.log("this is bar");
-}
+// bar();
+// function bar() {
+//   console.log("this is bar");
+// }
 
-foo();
-const foo = function () {
-  console.log("this is foo");
+// foo();
+// const foo = function () {
+//   console.log("this is foo");
+// };
+
+// Take a look at the following code snippet. Use call to invoke the add method
+// but with foo as execution context. What will this return?
+
+let foo = {
+  a: 1,
+  b: 2,
 };
+
+let bar = {
+  a: "abc",
+  b: "def",
+  add: function () {
+    return this.a + this.b;
+  },
+};
+
+console.log(bar.add.call(foo));
