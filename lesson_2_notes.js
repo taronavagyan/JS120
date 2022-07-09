c9816ce86230afd7e20f2c02a0f1f34a298dec02
Taron Avagyan <contact@taronavagyan.com>
2022-07-08 17:15:09 -0700

    PP execution context

--- lesson_2_notes.js
+++ lesson_2_notes.js
@@ -58,9 +58,8 @@ const TESgames = {
   titles: ["Arena", "Daggerfall", "Morrowind", "Oblivion", "Skyrim"],
   seriesTitle: "The Elder Scrolls",
   listGames: function () {
-    let self = this;
-    this.titles.forEach(function (title) {
-      console.log(self.seriesTitle + ": " + title);
+    this.titles.forEach((title) => {
+      console.log(this.seriesTitle + ": " + title);
     });
   },
 };
