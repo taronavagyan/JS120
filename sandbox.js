eb74fdb5504dc9f28870dcd4c40597d80a53ee66
Taron Avagyan <contact@taronavagyan.com>
2022-07-08 17:44:43 -0700

    test

--- sandbox.js
+++ sandbox.js
@@ -10,6 +10,7 @@ let baz = Object.create(foo);
 baz.qux();
 */
 
+/*
 let abc = { foo: 1, bar: 2 };
 let pqr = Object.create(abc);
 pqr.qux = 3;
@@ -21,3 +22,56 @@ console.log(abc.hasOwnProperty("qux"));
 console.log(pqr.hasOwnProperty("foo"));
 console.log(pqr.hasOwnProperty("bar"));
 console.log(pqr.hasOwnProperty("qux"));
+*/
+
+/*
+let xyz = {};
+
+console.log(Object.getPrototypeOf(xyz));
+*/
+
+/*
+console.log(Object.getPrototypeOf({}));
+console.log(Object.prototype);
+console.log(Object.getPrototypeOf({ a: 1, b: 2 }));
+*/
+
+/*
+let foo = { bar: 1, xyz: 3 };
+let baz = Object.create(foo);
+baz.qux = "Why not?";
+
+console.log(Object.keys(baz).toString());
+
+for (let prop in baz) {
+  if (baz.hasOwnProperty(prop)) {
+    console.log(prop);
+  }
+}
+
+Object.keys(baz).forEach((prop) => console.log(prop));
+
+Object.keys(baz).forEach((prop) => {
+  if (baz.hasOwnProperty(prop)) {
+    console.log(prop);
+  }
+});
+*/
+
+const OPERATIONS = {
+  "+": (num1, num2) => num1 + num2,
+  "-": (num1, num2) => num1 - num2,
+  "*": (num1, num2) => num1 * num2,
+  "/": (num1, num2) => num1 / num2,
+};
+
+let getOperation = (operation) => OPERATIONS[operation];
+
+let compute = function (operation, num1, num2) {
+  return operation(num1, num2);
+};
+
+console.log(compute(getOperation("/", 18, 6)) === 3);
+// console.log(compute("*", 2, 8) === 16);
+console.log(compute(getOperation("+"), 5, 9) === 14);
+console.log(compute(getOperation("%"), 9, 4) === 5);
