811f1219a41a86efec7779ee38e32babe96baeb1
Taron Avagyan <contact@taronavagyan.com>
2022-07-11 18:04:12 -0700

    invoice baseline

--- lesson_3_notes.js
+++ lesson_3_notes.js
@@ -1,6 +1,23 @@
-function makeObj() {
-  return {
-    propA: 10,
-    propB: 20,
-  };
-}
+// function makeObj() {
+//   return {
+//     propA: 10,
+//     propB: 20,
+//   };
+// }
+
+let invoice = {
+  phone: 3000,
+  internet: 6500,
+};
+
+let payment = {
+  phone: 1300,
+  internet: 5500,
+};
+
+let invoiceTotal = invoice.phone + invoice.internet;
+let paymentTotal = payment.phone + payment.internet;
+let remainingDue = invoiceTotal - paymentTotal;
+
+console.log(paymentTotal);
+console.log(remainingDue);
