f56fcf58c1793ddc37eafdbb0100d84d5939cbea
Taron Avagyan <contact@taronavagyan.com>
2022-07-12 13:17:24 -0700

    object args

--- constructor_practice.js
+++ constructor_practice.js
@@ -1,18 +1,30 @@
-function Car(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.started = false;
+let civicArgs = {
+  make: "Honda",
+  model: "Civic",
+  year: 2016,
+  color: "black",
+  passengers: 5,
+  convertible: false,
+  mileage: 16000,
+};
 
-  this.start = function () {
+function Car(args) {
+  Object.assign(this, args);
+
+  this.drive = function () {
     this.started = true;
-  };
-
-  this.stop = function () {
-    this.started = false;
   };
 }
 
-let corolla = new Car("Toyota", "Corolla", 2016);
-let leaf = new Car("Nissan", "LEAF", 2018);
-let nova = new Car("Chevrolet", "Nova", 1974);
+let civic = new Car(civicArgs);
+
+if (civic instanceof Car) console.log("I'm a car!");
+
+// function Lizard() {
+//   this.scamper = function () {
+//     console.log("I'm scampering!");
+//   };
+// }
+
+// let lizzy = new Lizard();
+// lizzy.scamper(); // ?
