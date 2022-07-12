e5b43e491d2f07b6b3c2935a38fdc5b560a9c546
Taron Avagyan <contact@taronavagyan.com>
2022-07-11 18:22:57 -0700

    payment function factory

--- lesson_3_notes.js
+++ lesson_3_notes.js
@@ -7,8 +7,8 @@
 
 function createInvoice(services = {}) {
   return {
-    phone: services.hasOwnProperty("phone") ? services.phone : 3000,
-    internet: services.hasOwnProperty("internet") ? services.internet : 5500,
+    phone: services.phone || 3000,
+    internet: services.internet || 5500,
 
     total() {
       return this.phone + this.internet;
@@ -26,14 +26,47 @@ function invoiceTotal(invoices) {
   return total;
 }
 
-let invoices = [];
-invoices.push(createInvoice());
-invoices.push(createInvoice({ internet: 6500 }));
-invoices.push(createInvoice({ phone: 2000 }));
-invoices.push(
-  createInvoice({
+function createPayment(services = {}) {
+  return {
+    phone: services.phone || 0,
+    internet: services.internet || 0,
+    amount: services.amount,
+
+    total() {
+      return this.amount || this.phone + this.internet;
+    },
+  };
+}
+
+function paymentTotal(payments) {
+  return payments.reduce((sum, payment) => sum + payment.total(), 0);
+}
+
+let payments = [];
+payments.push(createPayment());
+payments.push(
+  createPayment({
+    internet: 6500,
+  })
+);
+
+payments.push(
+  createPayment({
+    phone: 2000,
+  })
+);
+
+payments.push(
+  createPayment({
     phone: 1000,
     internet: 4500,
   })
 );
-console.log(invoiceTotal(invoices));
+
+payments.push(
+  createPayment({
+    amount: 10000,
+  })
+);
+
+console.log(paymentTotal(payments)); // => 24000
