6554ea6df80300969312f0a8f6d0747b12cf8583
Taron Avagyan <contact@taronavagyan.com>
2022-07-14 11:47:53 -0700

    quiz notes

--- lesson_3_notes.js
+++ lesson_3_notes.js
@@ -5,6 +5,7 @@
 //   };
 // }
 
+/*
 function createInvoice(services = {}) {
   return {
     phone: services.phone || 3000,
@@ -71,3 +72,56 @@ let payment3 = createPayment({ phone: 1000 });
 invoice.addPayment(payment1);
 invoice.addPayments([payment2, payment3]);
 console.log(invoice.amountDue()); // this should return 0
+*/
+
+// const Animal = function (species) {
+//   this.species = species;
+//   return species;
+// };
+
+// Animal.prototype.sleep = function () {
+//   console.log(`The ${this.species} is sleeping`);
+// };
+
+// let lion = new Animal("Panthera leo");
+// lion.sleep(); // TypeError
+// console.log(typeof lion);
+
+// function Dog() {}
+
+// function Pet(type) {
+//   if (type === "dog") {
+//     return new Dog();
+//   } else if (type === "lion") {
+//     return "not a pet!";
+//   }
+// }
+
+// let dog = new Pet("dog");
+// let lion = new Pet("lion");
+// let cat = new Pet("cat");
+
+// console.log([dog instanceof Dog, lion instanceof Pet, cat instanceof Pet]);
+
+// let str1 = new String("abc");
+// let str2 = String("abc");
+
+// console.log(str1 === str2); // => false
+
+// console.log(typeof str1, typeof str2);
+
+// function convertCase(char) {
+//   return char === char.toUpperCase() ?
+//  char.toLowerCase() : char.toUpperCase();
+// }
+
+// let str = "Naveed Fida";
+
+// str = str.split("").map(convertCase).join("");
+// console.log(str);
+
+class Foo {
+  // omitted code
+}
+
+console.log(typeof Foo);
