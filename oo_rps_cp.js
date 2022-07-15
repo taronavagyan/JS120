fb8fedad51c4cdefe805cc8a1b144fdff27d4a36
Taron Avagyan <contact@taronavagyan.com>
2022-07-15 12:17:37 -0700

    constructors for player, human, & computer

--- oo_rps_cp.js
+++ oo_rps_cp.js
@@ -1,50 +1,39 @@
 let readline = require("readline-sync");
 
-function createPlayer() {
-  return {
-    move: null,
-  };
+function Player() {
+  this.move = null;
 }
 
-function createComputer() {
-  let playerObject = createPlayer();
-
-  let computerObject = {
-    choose() {
-      const choices = ["rock", "paper", "scissors"];
-      let randomIndex = Math.floor(Math.random() * choices.length);
-      this.move = choices[randomIndex];
-    },
-  };
-
-  return Object.assign(playerObject, computerObject);
+function Human() {
+  Player.call(this);
 }
 
-function createHuman() {
-  let playerObject = createPlayer();
-  let humanObject = {
-    choose() {
-      let choice;
+Human.prototype.choose = function () {
+  let choice;
 
-      while (true) {
-        console.log("Please choose rock, paper, or scissors.");
-        choice = readline.question();
-        if (["rock", "paper", "scissors"].includes(choice)) break;
-        console.log("Sorry, invalid choice.");
-      }
+  while (true) {
+    console.log("Please choose rock, paper, or scissors.");
+    choice = readline.question();
+    if (["rock", "paper", "scissors"].includes(choice)) break;
+    console.log("Sorry, invalid choice.");
+  }
 
-      this.move = choice;
-    },
-  };
+  this.move = choice;
+};
 
-  return Object.assign(playerObject, humanObject);
+function Computer() {
+  Player.call(this);
 }
 
-// CONSTRUCTORS
+Computer.prototype.choose = function () {
+  const choices = ["rock", "paper", "scissors"];
+  let randomIndex = Math.floor(Math.random() * choices.length);
+  this.move = choices[randomIndex];
+};
 
 function RPSGame() {
-  this.human = createHuman();
-  this.computer = createComputer();
+  this.human = new Human();
+  this.computer = new Computer();
 }
 
 RPSGame.prototype = {
