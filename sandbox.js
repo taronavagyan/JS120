d1847e547a5896c6bfae0ada0d484bf799bad75c
Taron Avagyan <contact@taronavagyan.com>
2022-07-16 15:10:58 -0700

    .

--- sandbox.js
+++ sandbox.js
@@ -271,3 +271,45 @@ cats.foo();
 // 2: Pudding
 // 3: Fluffy
 */
+
+function Person(name) {
+  this.name = name;
+  this.school = undefined;
+}
+
+Person.prototype.speak = function () {
+  return `Hello, my name is ${this.name}.`;
+};
+
+// missing code
+//
+function Child(name, school) {
+  Person.call(this, name);
+  this.school = school;
+}
+
+Child.prototype = Object.create(Person.prototype);
+//
+
+Child.prototype.learn = function () {
+  return "I'm going to school!";
+};
+
+let child = new Child("Suzy", "PS 33");
+console.log(child instanceof Child); // true
+console.log(child instanceof Person); // true
+console.log(Object.getPrototypeOf(child) === Child.prototype); // true
+console.log(Object.getPrototypeOf(child).constructor === Child); // true
+console.log(child.school === "PS 33"); // true
+console.log(child.learn() === "I'm going to school!"); // true
+console.log(child.speak() === "Hello, my name is Suzy."); // true
+console.log();
+
+let person = new Person("Pete");
+console.log(person instanceof Child === false); // true
+console.log(person instanceof Person); // true
+console.log(Object.getPrototypeOf(person) === Person.prototype); // true
+console.log(Object.getPrototypeOf(person).constructor === Person); // true
+console.log(person.school === undefined); // true
+console.log(person.speak() === "Hello, my name is Pete."); // true
+console.log(person.learn === undefined);
