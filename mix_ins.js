999a1b45e1b3ff6502605dcd62177cb158d01cdc
Taron Avagyan <contact@taronavagyan.com>
2022-07-16 14:16:30 -0700

    #2

--- mix_ins.js
+++ mix_ins.js
@@ -2,27 +2,82 @@
 // as a mix-in to make them goFast? How can you check whether your Car or Truck
 // can now go fast?
 
-const Speed = {
-  goFast() {
-    console.log(`I'm a ${this.constructor.name} and going super fast!`);
+// const Speed = {
+//   goFast() {
+//     console.log(`I'm a ${this.constructor.name} and going super fast!`);
+//   },
+// };
+
+// class Car {
+//   goSlow() {
+//     console.log(`I'm safe and driving slow.`);
+//   }
+// }
+// Object.assign(Car.prototype, Speed);
+// let car = new Car();
+// car.goFast();
+
+// class Truck {
+//   goVerySlow() {
+//     console.log(`I'm a heavy truck and like going very slow.`);
+//   }
+// }
+
+// Object.assign(Truck.prototype, Speed);
+// let truck = new Truck();
+// truck.goFast();
+
+// 2.
+
+const Movable = {
+  range() {
+    return this.fuelCap * this.fuelEfficiency;
   },
 };
 
-class Car {
-  goSlow() {
-    console.log(`I'm safe and driving slow.`);
+class WheeledVehicle {
+  constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
+    this.fuelEfficiency = kmTravelledPerLiter;
+    this.fuelCap = fuelCapInLiter;
+    this.tires = tirePressure;
+  }
+
+  tirePressure(tireIdx) {
+    return this.tires[tireIdx];
+  }
+
+  inflateTire(tireIdx, pressure) {
+    this.tires[tireIdx] = pressure;
   }
 }
-Object.assign(Car.prototype, Speed);
-let car = new Car();
-car.goFast();
 
-class Truck {
-  goVerySlow() {
-    console.log(`I'm a heavy truck and like going very slow.`);
+Object.assign(WheeledVehicle.prototype, Movable);
+
+class Auto extends WheeledVehicle {
+  constructor() {
+    // the array represents tire pressure for four tires
+    super([30, 30, 32, 32], 50, 25.0);
   }
 }
 
-Object.assign(Truck.prototype, Speed);
-let truck = new Truck();
-truck.goFast();
+class Motorcycle extends WheeledVehicle {
+  constructor() {
+    // array represents tire pressure for two tires
+    super([20, 20], 80, 8.0);
+  }
+}
+
+class Catamaran {
+  constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
+    // catamaran specific logic
+
+    this.propellerCount = propellerCount;
+    this.hullCount = hullCount;
+    this.fuelEfficiency = kmTravelledPerLiter;
+    this.fuelCap = fuelCapInLiter;
+  }
+}
+
+Object.assign(Catamaran.prototype, Movable);
+let boat = new Catamaran(2, 2, 3, 4);
+console.log(boat.range());
