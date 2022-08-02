faca7a3bc7213c704d7549ac54b83cac731d1e5e
Taron Avagyan <contact@taronavagyan.com>
2022-08-02 13:05:38 -0700

    #5

--- exercises/easy.js
+++ exercises/easy.js
@@ -80,6 +80,7 @@ console.log(fakeCat.speaks());
 
 // 4.
 
+/*
 class Pet {
   constructor(name, age) {
     this.name = name;
@@ -94,7 +95,8 @@ class Cat extends Pet {
   }
 
   info() {
-    return `My cat ${this.name} is ${this.age} years old and has ${this.furColor} fur.`;
+    return `My cat ${this.name} is ${this.age} years old and has ${this.
+      furColor} fur.`;
   }
 }
 
@@ -106,3 +108,57 @@ console.log(butterscotch.info());
 
 // My cat Pudding is 7 years old and has black and white fur.
 // My cat Butterscotch is 10 years old and has tan and white fur.
+*/
+
+// 5.
+
+/*
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old and 
+    ${this.status}.`;
+  }
+}
+
+class Cat extends Animal {
+  static LEGS = 4;
+  static SPECIES = "cat";
+
+  constructor(name, age, status) {
+    super(name, age, Cat.LEGS, Cat.SPECIES, status);
+  }
+
+  introduce() {
+    return super.introduce() + " Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  static LEGS = 4;
+  static SPECIES = "dog";
+
+  constructor(name, age, status, master) {
+    super(name, age, Dog.LEGS, Dog.SPECIES, status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}! Woof, woof!`;
+  }
+}
+
+let marlow = new Cat("Pepe", 2, "happy");
+console.log(marlow.introduce());
+
+let jack = new Dog("Jack", 1, "excited", "William");
+console.log(jack.introduce());
+console.log(jack.greetMaster());
+*/
