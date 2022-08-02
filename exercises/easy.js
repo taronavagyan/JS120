4ff98940347718170bc95dc00eafdeff9e658ff9
Taron Avagyan <contact@taronavagyan.com>
2022-08-02 12:46:50 -0700

    #2

--- exercises/easy.js
+++ exercises/easy.js
@@ -25,3 +25,36 @@ console.log(rect.getWidth()); // 4
 console.log(rect.getLength()); // 5
 console.log(rect.getArea()); // 20
 */
+
+// 2.
+
+/*
+class Rectangle {
+  constructor(width, length) {
+    this.width = width;
+    this.length = length;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getArea() {
+    return this.width * this.length;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(sideLength) {
+    super(sideLength, sideLength);
+  }
+}
+
+let square = new Square(5);
+
+console.log(`area of square = ${square.getArea()}`); // area of square = 25
+*/
